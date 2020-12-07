467d74acf9b964d8bad21e4e32a01bd768247bbd
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-07 19:57:52 +0000

    0x00. Python - Hello, World

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,18 +11,20 @@
 int check_cycle(listint_t *list)
 {
 	listint_t *current = list;
-	listint_t *future = list->next;
+	listint_t *future = list;
 
 	if (list == NULL || list->next == NULL)
 		return (0);
 
+	if (current == future->next)
+		return (1);
+
 	while (current && future)
 	{
-		if (current == future)
-			return (1);
-
 		current = current->next;
 		future = future->next->next;
+		if (current == future)
+			return (1);
 	}
 	return (0);
 }
