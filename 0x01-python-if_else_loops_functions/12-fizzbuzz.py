83da8f0b7e00a9ba7422569e1d6b8b77b5f75c42
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-08 13:47:44 -0300

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 def fizzbuzz():
-i = 1
-while i <= 100:
-    if i % 15 == 0:
-        print("FizzBuzz", end="")
-    elif i % 3 == 0:
-        print("Fizz", end="")
-    elif (i % 5 == 0):
-        print("Buzz", end="")
-    else:
-        print(i, end="")
 
-    if i != 100:
-        print(" ", end="")
-    else:
-        print("")
-    i += 1
+    i = 1
+    while i <= 100:
+        if i % 15 == 0:
+            print("FizzBuzz", end="")
+        elif i % 3 == 0:
+            print("Fizz", end="")
+        elif (i % 5 == 0):
+            print("Buzz", end="")
+        else:
+            print(i, end="")
+
+            if i != 100:
+                print(" ", end="")
+            else:
+                print("")
+        i += 1
