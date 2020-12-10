0aa483f27d2e9e2d52d84fb558883fa21a5f87f9
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-10 15:57:59 -0300

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,28 +3,26 @@ if __name__ == "__main__":
     import sys
     from calculator_1 import *
 
-    leng = len(sys.argv)
-
-    if leng != 4:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
         if sys.argv[2] == "+":
             print("{} + {} = {:d}"
                   .format(sys.argv[1], sys.argv[3],
-                          int(sys.argv[1]) + int(sys.argv[3])))
+                          add(int(sys.argv[1]), int(sys.argv[3]))))
         elif sys.argv[2] == "-":
             print("{} - {} = {:d}"
                   .format(sys.argv[1], sys.argv[3],
-                          int(sys.argv[1]) - int(sys.argv[3])))
+                          sub(int(sys.argv[1]), int(sys.argv[3]))))
         elif sys.argv[2] == "*":
             print("{} * {} = {:d}"
                   .format(sys.argv[1], sys.argv[3],
-                          int(sys.argv[1]) * int(sys.argv[3])))
+                          mul(int(sys.argv[1]), int(sys.argv[3]))))
         elif sys.argv[2] == "/":
-            print("{} / {} = {:d}"
+            print("{} / {} = {:.2f}"
                   .format(sys.argv[1], sys.argv[3],
-                          int(sys.argv[1]) / int(sys.argv[3])))
+                          div(int(sys.argv[1]), int(sys.argv[3]))))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
