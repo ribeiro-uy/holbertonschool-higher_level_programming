884ac1a759930ad87e3bf48cacdebe42e30025b9
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-10 12:13:18 -0300

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
 
-    import add_0
+    from add_0 import add
 
     a = 1
     b = 2
 
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
