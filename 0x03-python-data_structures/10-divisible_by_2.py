e76ddbe15546ff7131b42e4f23b5aba9464cd702
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-15 14:36:04 -0300

    0x03-python-data_structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new_list = my_list[:]
     for number in range(len(my_list)):
-        if number % 2  == 0:
+        if number % 2 == 0:
             new_list[number] = True
         else:
             new_list[number] = False
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,4 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     for interger in range(len(my_list)):
-        print ("{:d}".format(my_list[interger]))
+        print("{:d}".format(my_list[interger]))
