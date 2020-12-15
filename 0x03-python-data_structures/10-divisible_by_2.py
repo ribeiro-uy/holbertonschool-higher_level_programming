f81027bf68ffae82d913878cf959f884fbbb1c40
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-15 15:02:32 -0300

    0x03-python-data_structures

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list[:]
-    for number in range(len(my_list)):
+    new_list = list(my_list)
+    for number in my_list:
         if number % 2 == 0:
             new_list[number] = True
         else:
