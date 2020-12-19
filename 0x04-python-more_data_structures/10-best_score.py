621f90b08da5224e202d616b09e9a92f1d25c6ee
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-19 19:22:49 -0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def best_score(my_dict):
-    if bool(my_dict):
-        return max(my_dict.items())[0]
-    return None
+def best_score(a_dictionary):
+    if a_dictionary:
+        return max(a_dictionary, key=lambda k: a_dictionary[k])
