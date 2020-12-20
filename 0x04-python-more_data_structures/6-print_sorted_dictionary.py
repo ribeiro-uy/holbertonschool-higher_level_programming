2f7ceefe9abe64b01808278921830e05bf967f2a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-20 09:40:26 -0300

    0x04-python-more_data_structures

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for k, v in sorted(a_dictionary.items()):
-        print ("{}: {}".format(k, v)
-    
+        print("{}: {}".format(k, v))
