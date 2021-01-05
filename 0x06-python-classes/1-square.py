2b2d1f61926dbf0dac273ebf9fa0b95a0a5f0ca1
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-05 15:59:12 -0300

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    A class Square
+    """
+    def __init__(self, size):
+        """
+        Args:
+        size (int): Size of square
+        """
+        self.__size = size
