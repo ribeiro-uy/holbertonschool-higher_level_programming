307766061b66318386ae6b3c6671440053844fee
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-05 19:37:45 -0300

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,6 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-
     def area(self):
         """
         Returns the current square area
