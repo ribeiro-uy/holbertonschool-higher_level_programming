46d44e0290c89cbf510b3105561ac9715b65c876
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-05 16:12:57 -0300

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A class Square that defines a square by: (based on 0-square.py)
+"""
+
+
+class Square:
+    """
+    A class Square
+    """
+    def __init__(self, size):
+        """
+        Args:
+        size (int): Size of square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
