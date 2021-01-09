4fd5a0726b37092dc8da2c87eacc24ee58623292
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-09 22:41:46 +0000

    6-max_integer_test.py

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -35,3 +35,7 @@ class TestMaxInteger(unittest.TestCase):
         Test list of one element.
         """
         self.assertEqual(max_integer([7]), 7)
+
+    def test_char(self):
+        """Test if list elements are character characters."""
+        self.assertEqual(max_integer(["a", "b", "c"]), "c")
