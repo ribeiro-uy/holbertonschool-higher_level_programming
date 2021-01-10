3e6e097bb1dcb0fb527a662aa8ec421b2333cd44
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-10 12:23:52 -0300

    0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,21 +12,10 @@ def add_integer(a, b=98):
     """
     if type(a) is not int and type(a) is not float:
         raise TypeError("a must be an integer")
-    if type(a) is bool:
-        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("b must be an integer")
-    if type(b) is bool:
         raise TypeError("b must be an integer")
     if a+1 == a:
         raise OverflowError("cannot convert float infinity to integer")
     if b+1 == b:
-        raise OverflowError("b is too big")
-    
-    #cast a and b into intergers
-    a = int(a)
-    b = int(b)
-    try:
-        return a + b
-    except SyntaxError:
-        raise SyntaxError("cualca")
+        raise OverflowError("cannot convert float infinity to integer")
+    return int(a) + int(b)
