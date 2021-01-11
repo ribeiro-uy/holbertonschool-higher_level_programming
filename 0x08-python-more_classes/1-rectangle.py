ee7b86c74569918205eb6eb86248b268ba7f9d0d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-11 18:43:26 -0300

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         Constructor
         """
 
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
