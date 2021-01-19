a81e141e185faf5198d34664e4147aed62f9592f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-19 16:51:30 -0300

    tests folder

---
+++ 0x0A-python-inheritance/tests/0-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+lookup = __import__('0-lookup').lookup
+
+class MyClass1(object):
+    pass
+
+class MyClass2(object):
+    my_attr1 = 3
+    def my_meth(self):
+        pass
+
+print(lookup(MyClass1))
+print()
+print(lookup(MyClass2))
+print()
+print(lookup(int))
