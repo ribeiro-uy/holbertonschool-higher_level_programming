ad5bc46daba11b0a5385c49168079050bf712552
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-19 19:25:53 -0300

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Project: 0x0A-python-inheritance.
+Task: 7
+"""
+
+
+class BaseGeometry:
+    """
+    Based on 6-base_geometry.py
+    """
+
+    def area(self):
+        """
+        Public instance method: def area(self):
+        that raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method: def integer_validator(self, name, value):
+        that validates value.
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
