d31439629b781ee94424258d69e10c12343d5b3e
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-19 17:33:42 -0300

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+Project: 0x0A-python-inheritance.
+Task: 10
+"""
+
+
+class BaseGeometry:
+    """
+    Based on 7-base_geometry.py
+    """
+
+    def area(self):
+        """
+        Public instance method: def area(self):
+        that raises an Exception with the message area() is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method: def integer_validator(self, name, value):
+        that validates value.
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    New class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+    """
+    def __init__(self, width, height):
+        """
+        Constructor
+        """
+
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", self.__width)
+        super().integer_validator("height", self.__height)
+
+    def __str__(self):
+        """
+        str method
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Area of the Rectangle
+        """
+        return self.__width * self.__height
+
+
+class Square(Rectangle):
+    """
+    Square that inherits from Rectangle (9-rectangle.py):
+    """
+
+    def __init__(self, size):
+        """
+        Constructor
+        """
+        self.__size = size
+        super().integer_validator("size", self.__size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        """
+        str method
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """
+        Area of the Square
+        """
+        return self.__size * self.__size
