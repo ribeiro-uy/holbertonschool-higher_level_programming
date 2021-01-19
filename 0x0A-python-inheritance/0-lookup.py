7491a12d2cdd92ef3feb5584909369ed3dde1eec
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-19 11:52:03 -0300

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Project: 0x0A-python-inheritance
+Task: 0
+"""
+
+
+def lookup(obj):
+    """
+    Returns the list of available attributes and methods of an object
+    """
+    return list(dir(obj))
