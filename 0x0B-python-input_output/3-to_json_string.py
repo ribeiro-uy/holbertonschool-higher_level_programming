c27b335bc8fbf8a9130bab44f9e751f4a076f201
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-20 23:03:09 +0000

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Project: 0x0B. Python - Input/Output
+Task: 3
+"""
+import json
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
