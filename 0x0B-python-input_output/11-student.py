f7fbfc73ac286f5f5503bbd9aa03f9b61c23f81a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-20 23:05:17 +0000

    11-student.py

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Project: 0x0B. Python - Input/Output
+Task: 11
+"""
+
+
+class Student:
+    """
+    Class that define students
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instanceo
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for item in attrs:
+                if item in self.__dict__:
+                    new_dict[item] = self.__dict__[item]
+            return new_dict
+        
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of the Student instance 
+        """
+        for item in json:
+            self.__dict__[item] = json[item]
+        
