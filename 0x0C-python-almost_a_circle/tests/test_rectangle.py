261aaa128b6ee244c35eb72eb6a0f15c2aa42abe
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-26 20:38:36 -0300

    tests

---
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python3
+"""test module for rectangle"""
+
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """testing the Rectangle class and its methods"""
+    
+    def test_inst_methods(self):
+        """test methods"""
+        test_inst = Rectangle(2, 3, 0, 0, 1)
+        """test area of rectangle"""
+        self.assertEqual(test_inst.area(), 6)
+        """test str"""
+        self.assertEqual(test_inst.__str__(), '[Rectangle] (1) 0/0 - 2/3')
+        """test to_dictionary"""
+        self.assertEqual(test_inst.to_dictionary(),
+                         {'id': 1, 'width': 2, 'height': 3, 'x': 0, 'y': 0})
+        
+    def test_negatives(self):
+        """test negative values"""
+        with self.assertRaises(ValueError):
+            """negative width"""
+            fail_inst = Rectangle(-2, 3, 3, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative height"""
+            fail_inst = Rectangle(2, -3, 3, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative x"""
+            fail_inst = Rectangle(2, 3, -1, 0, 0)
+        with self.assertRaises(ValueError):
+            """negative y"""
+            fail_inst = Rectangle(-2, 3, 0, -2, 0)
+
+    def test_not_int(self):
+        """test values without ints"""
+        with self.assertRaises(TypeError):
+            """on width"""
+            fail_inst = Rectangle('testStr', 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on height"""
+            fail_inst = Rectangle(3, '3', 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on x"""
+            fail_inst = Rectangle(1, 3, '0', 0, 1)
+        with self.assertRaises(TypeError):
+            """on y"""
+            fail_inst = Rectangle(2, 3, 0, '0', 1)
+            
+    def test_None_values(self):
+        """test None values"""
+        with self.assertRaises(TypeError):
+            """on width"""
+            fail_inst = Rectangle(None, 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on height"""
+            fail_inst = Rectangle(2, None, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            """on x"""
+            fail_inst = Rectangle(2, 3, None, 0, 1)
+        with self.assertRaises(TypeError):
+            """on y"""
+            fail_inst = Rectangle(2, 5, 0, None, 1)
+            
+    def test_arg_number(self):
+        """test less that 3 args and more than 5 args"""
+        with self.assertRaises(TypeError):
+            inst = Rectangle(3, 3, 0, 0, 1, 5)
+        with self.assertRaises(TypeError):
+            inst = Rectangle(1)
+
+    def test_update(self):
+        """test update method"""
+        inst_update = Rectangle(1, 1, 0, 0, 1)
+        self.assertEqual(inst_update.to_dictionary(),
+                         {'id': 1, 'width': 1, 'height': 1, 'x': 0, 'y': 0})
+        inst_update.update(13, 3, 2, 0, 0)
+        self.assertEqual(inst_update.to_dictionary(),
+                         {'id': 13, 'width': 3, 'height': 2, 'x': 0, 'y': 0})
+        
+    def test_update_failure(self):
+        """test failures of update method"""
+        inst_up = Rectangle(1, 1, 1, 1)
+        with self.assertRaises(TypeError):
+            """more than five args"""
+            inst_up.update(1, 2, 3, 3, 1, 2)
+        with self.assertRaises(TypeError):
+            """wrong data type"""
+            inst_up.update(1, 1, 'test', [1], 1)
+            
+    def test_float_values(self):
+        """Test values as floats."""
+        with self.assertRaises(TypeError):
+            # Float as width
+            fail_inst = Rectangle(8.1, 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float as height
+            fail_inst = Rectangle(3, 2.4, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float as x
+            fail_inst = Rectangle(1, 3, 6.3, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float as y
+            fail_inst = Rectangle(2, 3, 0, 4.2, 1)
+            # Float('inf') as value
+        with self.assertRaises(TypeError):
+            # Float('inf') as width
+            fail_inst = Rectangle(float('inf'), 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('inf') as height
+            fail_inst = Rectangle(3, float('inf'), 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('inf') as x
+            fail_inst = Rectangle(1, 3, float('inf'), 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('inf') as y
+            fail_inst = Rectangle(2, 3, 0, float('inf'), 1)
+            # Float('NaN') as value
+        with self.assertRaises(TypeError):
+            # Float('NaN') as width
+            fail_inst = Rectangle(float('NaN'), 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('NaN') as height
+            fail_inst = Rectangle(3, float('NaN'), 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('NaN') as x
+            fail_inst = Rectangle(1, 3, float('NaN'), 0, 1)
+        with self.assertRaises(TypeError):
+            # Float('NaN') as y
+            fail_inst = Rectangle(2, 3, 0, float('NaN'), 1)
+            
+    def test_boolean_values(self):
+        """Test values as floats."""
+        with self.assertRaises(TypeError):
+            # Boolean as width
+            fail_inst = Rectangle(True, 3, 0, 0, 1)
+            fail_inst = Rectangle(False, 3, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Boolean as height
+            fail_inst = Rectangle(3, True, 0, 0, 1)
+            fail_inst = Rectangle(3, False, 0, 0, 1)
+        with self.assertRaises(TypeError):
+            # Boolean as x
+            fail_inst = Rectangle(1, 3, True, 0, 1)
+            fail_inst = Rectangle(1, 3, False, 0, 1)
+        with self.assertRaises(TypeError):
+            # Boolean as y
+            fail_inst = Rectangle(2, 3, 0, True, 1)
+            fail_inst = Rectangle(2, 3, 0, False, 1)
+        
+    def test_zero_values(self):
+        """Test passing 0."""
+        with self.assertRaises(ValueError):
+            # on width
+            fail_inst = Rectangle(0, 3, 0, 0, 1)
+        with self.assertRaises(ValueError):
+            # on height
+            fail_inst = Rectangle(2, 0, 0, 0, 1)
+            
+    def test_kwargs(self):
+        """test update with kwargs"""
+        inst_update = Rectangle(1, 1, 0, 0, 1)
+        self.assertEqual(inst_update.to_dictionary(),
+                         {'id': 1, 'width': 1, 'height': 1, 'x': 0, 'y': 0})
+        inst_update.update(id=13, width=3, height=2, x=0, y=0)
+        self.assertEqual(inst_update.to_dictionary(),
+                         {'id': 13, 'width': 3, 'height': 2, 'x': 0, 'y': 0})
+        inst_update.update(13, 1, 1, width=3, height=2)
+        self.assertEqual(inst_update.to_dictionary(),
+                         {'id': 13, 'width': 1, 'height': 1, 'x': 0, 'y': 0})
+        
+    def test_update_failure(self):
+        """test failures of update method with kwargs"""
+        inst_up = Rectangle(1, 1, 1, 1)
+        with self.assertRaises(TypeError):
+            """wrong data type"""
+            inst_up.update(id=1, height=[], width='test', x=1, y=1)
+        with self.assertRaises(ValueError):
+            """negative"""
+            inst_up.update(id=1, height=-1, width=1, x=1, y=1)
+        with self.assertRaises(ValueError):
+            """0 on height"""
+            inst_up.update(id=1, height=0, width=1, x=1, y=1)
+            
+    def test_display(self):
+        """test the display function"""
+        import io
+        import contextlib
+        
+        inst = Rectangle(3, 4, 0, 0, 1)
+        with io.StringIO() as fd:
+            with contextlib.redirect_stdout(fd):
+                inst.display()
+                rec = fd.getvalue()
+        self.assertEqual(rec, '###\n###\n###\n###\n')
+        
+if __name__ == '__main__':
+    unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python3
+"""module of tests for square"""
+
+
+import unittest
+from models.square import Square
+import pep8
+
+
+class TestCodeFormat(unittest.TestCase):
+    """Test for pep8"""
+    def test_pep8_conformance(self):
+        """Test that we conform to PEP8."""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['square.py'])
+        self.assertEqual(result.total_errors, 1,
+                         "Found code style errors (and warnings).")
+
+
+class Test_Square(unittest.TestCase):
+    """Tests for square"""
+    def no_doc(item):
+        """A decorator to add the no-doc docstring
+        objects that don't need any other documentation"""
+        t = "class" if inspect.isclass(item) else "function"
+        item.__doc__ = "This {} intentionally has no documentation".format(t)
+        
+    def test_area(self):
+        """Test area of rectangle"""
+        test_inst = Square(2, 2)
+        self.assertEqual(test_inst.area(), 4)
+        
+    def test_square(self):
+        """Test Square without parameters"""
+        square = Square(1, 2, 3)
+        square2 = Square(2)
+        square3 = Square(2, 3)
+        square4 = Square(2, 3, 4, 5)
+        
+    def test_wrong_parameter(self):
+        """Test for non int arguments in square"""
+        with self.assertRaises(TypeError):
+            square = Square([1, 2, 3, 4], "5")
+            square = Square(1, None)
+            square = Square()
+            square = Square(None)
+            square = Square([1, 2, 3])
+            square = Square("str", 2)
+            square = Square({"id": 1, "od": 2})
+            
+    def test_size(self):
+        """Test for size values"""
+        square = Square(1, 2, 3)
+        with self.assertRaises(ValueError):
+            square.size = -1
+            square.size = 1e10000000
+            square.size = 0
+            
+    def test_none_size(self):
+        """Test for non int values in size"""
+        square = Square(1, 2, 3)
+        with self.assertRaises(TypeError):
+            square.size = None
+            square.size = True
+            square.size = "Atenea"
+            square.size = [1, 2, 3]
+            square.size = {"id": 1, "od": 2}
+            square.size = 2.34
+            square.size = (1, 2, 3)
+            square.size = float(NaN)
+            
+    def test_negative_square(self):
+        """Test for size negative value"""
+        with self.assertRaises(ValueError):
+            square = Square(-2)
+            
+    def test_0_square(self):
+        """Test for 0 value in size"""
+        with self.assertRaises(ValueError):
+            square = Square(0, 2, 3)
+            
+    def test_x_value(self):
+        """Test negative value in x"""
+        square = Square(1)
+        with self.assertRaises(ValueError):
+            square.x = -2
+            
+    def test_y_value(self):
+        """Test negative value in y"""
+        square = Square(1)
+        with self.assertRaises(ValueError):
+            square.y = -5
+            
+    def test_x_type(self):
+        """Test non int values in x"""
+        square = Square(2)
+        with self.assertRaises(TypeError):
+            square.x = [1, 2, 3]
+            square.x = {1, 2}
+            square.x = "2"
+            square.x = 3.14
+            square.x = None
+            square.x = True
+            square.x = {"id": 1, "od": 2}
+            square.x = (1, 2, 3)
+            square.x = float(NaN)
+            
+    def test_y_type(self):
+        """Test non int values in y"""
+        square = Square(2)
+        with self.assertRaises(TypeError):
+            square.y = [1, 2, 3]
+            square.y = {1, 2}
+            square.y = "2"
+            square.y = 3.14
+            square.y = None
+            square.y = True
+            square.y = {"id": 1, "od": 2}
+            square.y = (1, 2, 3)
+            square.y = float(NaN)
+            
+    def test_more_parameters(self):
+        """Test more than 4 arguments"""
+        with self.assertRaises(TypeError):
+            square = Square(2, 4, 5, 7, 8, 9)
+            
+    def test_str(self):
+        """Test str for square"""
+        square = Square(5, 0, 0, 4)
+        self.assertEqual(square.__str__(), '[Square] (4) 0/0 - 5')
+        
+    def test_update(self):
+        """Test update with correct values"""
+        square = Square(5)
+        square.update()
+        square.update(1, 2, y=3)
+        square.update(1, 12, id=4)
+        square.update(size=2, y=5)
+        
+    def test_update_wrong(self):
+        """Test update with value errors"""
+        square = Square(5)
+        with self.assertRaises(ValueError):
+            square.update(0)
+            square.update(-1, 2, 3)
+            square.update(size=-1, x=2, y=3)
+            square.update(size=1e1000000)
+            
+    def test_update_type(self):
+        """Test update with Type error"""
+        square = Square(4)
+        with self.assertRaises(TypeError):
+            square.update("ate", 2, 3)
+            square.update(1, 2, 3, 4, 5, 7)
+            square.update([1, 2, 3])
+            square.update({"id": 1, "od": 2})
+            square.update(2.3, 2, 3)
+            square.update((1, 2, 3))
+            square.update(None, 3, 4)
+            square.update(True, 3, 4)
+            square.update(float(NaN))
+            square.update(size=[1, 2, 3])
+            square.update(2, x="ate", y=3)
+            
+    def test_display(self):
+        """test the display function"""
+        import io
+        import contextlib
+        
+        inst = Square(3, 0, 0, 1)
+        with io.StringIO() as fd:
+            with contextlib.redirect_stdout(fd):
+                inst.display()
+                rec = fd.getvalue()
+        self.assertEqual(rec, '###\n###\n###\n')
+                
+if __name__ == '__main__':
+    unittest.main()
