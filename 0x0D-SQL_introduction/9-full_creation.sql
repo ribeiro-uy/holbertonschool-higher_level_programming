cdda17bdcd07d35c3085dc00095a3b7c8a9c37ff
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-01 16:13:54 -0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
--- Creates a table called first_table in the current database in your MySQL server.
+-- Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
        id INT,
        name VARCHAR(256) NOT NULL,
