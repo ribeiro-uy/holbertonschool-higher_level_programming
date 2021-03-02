ad984bb819c0a9a26ea0b68440f7ee93292f4514
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-02 12:25:12 -0300

    remove use

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,7 +5,6 @@
 --	 If the user user_0d_2 already exists, your script should not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON * . * TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
