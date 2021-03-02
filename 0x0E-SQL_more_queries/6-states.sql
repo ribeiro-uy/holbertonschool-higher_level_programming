fce59b65ca0439515530be0aad6c73b86d68bf0f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-02 14:36:23 -0300

    add unique, remove default

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- Creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 -- 	 states description:
---       	    id INT unique, auto generated, can’t be null and is a primary key
---            	    name VARCHAR(256) can’t be null
+--           id INT unique, auto generated, can’t be null and is a primary key
+--           name VARCHAR(256) can’t be null
 --       If the database hbtn_0d_usa already exists, your script should not fail
 --	 If the table states already exists, your script should not fail
 
