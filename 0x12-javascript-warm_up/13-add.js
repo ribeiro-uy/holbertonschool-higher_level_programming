3ec452282945082c68de0f67e8e3f7de984096f4
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-22 14:59:09 -0300

    gix indentation

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -4,7 +4,7 @@
 */
 
 function add (num1, num2) {
-    return (num1 + num2);
+  return (num1 + num2);
 }
 // With NodeJS modules, to make something public, you have to export it
 module.exports.add = add;
