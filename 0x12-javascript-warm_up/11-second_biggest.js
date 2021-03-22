9cab863d04747799fd1c98705141ab1f98ca9a0b
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-22 15:22:39 -0300

    add delete dublicate and order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,9 +5,18 @@
 
 const argument = [];
 const lastIndex = process.argv.length;
+let newArgument = [];
+
 process.argv.forEach((val, index) => {
   argument[index] = val;
 });
-argument.sort();
-console.log(parseInt(argument[lastIndex - 2]));
-// -2 because the length doesn't take the 0 index
+newArgument = [...new Set(argument)];
+newArgument.sort((a, b) => a - b);
+console.log('check newArgument:' + newArgument);
+if (lastIndex <= 2)
+  console.log(0);
+else if (lastIndex === 3)
+  console.log(0);
+else
+  console.log(parseInt(newArgument[lastIndex - 2]));
+  // -2 because the length doesn't take the 0 index
