905c0d9f72b7532de91638899e236815fe795c79
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 18:20:27 -0300

    task 11

---
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+  Task 11:
+  Script that imports an array and computes a new array.
+*/
+
+const list = require('./100-data');
+console.log(list);
+console.log(list.map((x, idx) => {return x * idx}));
