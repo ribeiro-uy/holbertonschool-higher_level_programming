37df4c3be5953b37eae453b09a5e37026c7550d0
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 15:11:52 -0300

    task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+  Task 8:
+  Function that returns the reversed version of a list
+*/
+
+exports.esrever = function (list) {
+
+  return list.map((item,idx) => list[list.length-1-idx])
+}
