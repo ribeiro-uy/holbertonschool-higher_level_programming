86829f36d5a2bbfb59ca68a0b0f041f4d295a9c2
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 13:34:04 -0300

    task 1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+  Task 1:
+  Empty class Rectangle that defines a rectangle:
+*/
+
+module.exports = class Rectangle {
+  // Constructor
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
