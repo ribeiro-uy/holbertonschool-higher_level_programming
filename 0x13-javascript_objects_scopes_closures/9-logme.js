bb119f36593a27d29adbfe79883f4de1566b26f8
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 18:08:16 -0300

    task 9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -4,7 +4,8 @@
   Function that prints the number of arguments already printed and the new argument value.
 */
 
+let numArguments = 0;
 exports.logMe = (item) => {
   console.log(`${num}: ${item}`);
-  num++;
+  numArguments++;
 };
