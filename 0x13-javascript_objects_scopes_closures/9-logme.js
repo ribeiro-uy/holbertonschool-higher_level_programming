b623ce6c25e91d5fc9e19c3965366e48e1890e54
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 18:06:47 -0300

    task 9

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/*
+  Task 9:
+  Function that prints the number of arguments already printed and the new argument value.
+*/
+
+exports.logMe = (item) => {
+  console.log(`${num}: ${item}`);
+  num++;
+};
