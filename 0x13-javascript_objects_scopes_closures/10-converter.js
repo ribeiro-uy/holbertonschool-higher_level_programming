fe42bb13130decb51d69a7a0f0ead670d6f8f70d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 18:10:53 -0300

    task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+  Task 10:
+  Converts a number from base 10 to another base passed as argument
+*/
+
+exports.converter = (base) => {
+    return num => num.toString(base);
+};
