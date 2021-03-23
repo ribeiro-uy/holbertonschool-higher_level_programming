a5cc2ecf5837ddf2f84008191ca9cb3adb1cc29f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-23 13:58:21 -0300

    task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/*
+  Task 5:
+  Class Square that defines a square and inherits from Rectangle of 4-rectangle
+*/
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  // Constructor
+  constructor (size) {
+  super(size, size)
+    }
+};
