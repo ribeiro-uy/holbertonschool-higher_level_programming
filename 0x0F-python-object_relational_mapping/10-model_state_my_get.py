e9cf88e00edb37e938ec6c0754bcac8dc08d2e73
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 16:45:09 +0000

    add first

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state_id = session.query(State).filter_by(name=argv[4])
+    state_id = session.query(State).filter_by(name=argv[4]).first()
     if state_id:
-        print(state_id[0].id)
+        print(state_id.id)
     else:
         print("Not found")
     session.close()
