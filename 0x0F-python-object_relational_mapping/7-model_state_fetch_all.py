f23ac5ddf506388bf313591d2bf786904b9474c7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 14:09:07 +0000

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    conection = engine.connect()
+    selection = conection.execute("Select * from states")
+#    for elements in selection:
+#        print(elements)
+    new_list = []
+    for elements in selection:
+       print("{}: {}".format(elements[0], elements[1]))
