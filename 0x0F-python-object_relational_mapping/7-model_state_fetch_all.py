9150202a55ca2f4c685b75fca8088c252b2a365d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 14:21:44 +0000

    fix pep8

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,8 +11,7 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     conection = engine.connect()
     selection = conection.execute("Select * from states")
     for elements in selection:
-       print("{}: {}".format(elements[0], elements[1]))
+        print("{}: {}".format(elements[0], elements[1]))
