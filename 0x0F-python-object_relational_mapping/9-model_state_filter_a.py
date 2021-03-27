10f8dc70e5c468a580e5aa13eb8cbea9a495b907
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 16:14:10 +0000

    fix pep8

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+Script that lists all State objects that contain
+the letter a from the database hbtn_0e_6_usa
 """
 
 from sys import argv
