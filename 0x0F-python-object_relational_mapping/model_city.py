393df247574382cb7e8a3800a20812249e53cb59
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 17:52:03 -0300

    remove import relationship

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,7 +6,6 @@ Class definition of a City and an instance Base = declarative_base()
 import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 Base = declarative_base()
 
