d45a0505411946c7759dcc15cd11edde1f744c36
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-03-27 15:09:20 +0000

    delete unnecesary variable

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -3,18 +3,16 @@
 script that lists all State objects from the database hbtn_0e_6_usa
 """
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
     for states in session.query(State):
         print("{}: {}".format(states.id, states.name))
