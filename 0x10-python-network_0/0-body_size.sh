426d3b5367cd6b2cdba80823b991e7fa1242a82d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-04-08 14:34:24 -0300

    task 0, first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sI $1 | grep Content-Length | cut -d " " -f2
+curl -sI "$1" | grep Content-Length | cut -d " " -f2
