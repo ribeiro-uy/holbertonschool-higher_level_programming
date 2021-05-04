281cd7e180e77dbdeebef0b12342a6415902fda5
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-04 17:42:15 -0300

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+request(process.argv[2], function (response, body) {
+  const tasks = (JSON.parse(body.body));
+  const tasksDone = {};
+  let taskCount = 0;
+  for (const task in tasks) {
+    const userID = tasks[task].userId;
+    if (!(userID in tasksDone)) {
+      taskCount = 0;
+    }
+    if (tasks[task].completed === true) {
+      tasCount++;
+    }
+    tasksDone[userID] = taskCount;
+  }
+  console.log(tasksDone);
+});
