0ff6be171486b9175cef2bc34e44f819b8278ab2
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-05 09:58:52 -0300

    fix indentation

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2] + "/?format=json"
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
 request(url, function (response, body) {
-    const film = (JSON.parse(body.body));
-    for (let id in film.characters) {
-	// console.log(film.characters[id]);
-	request(film.characters[id], function (response, body) {
-	    const character = (JSON.parse(body.body));
-	    console.log(character.name);
-	})
-    }
+  const film = (JSON.parse(body.body));
+  for (const id in film.characters) {
+    request(film.characters[id], function (response, body) {
+      const character = (JSON.parse(body.body));
+      console.log(character.name);
+    });
+  }
 });
-	
