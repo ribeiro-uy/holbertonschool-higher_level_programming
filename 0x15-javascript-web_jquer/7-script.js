f15bc1342ff71945e75ae9a1ed0de592049071b0
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-12 22:19:31 -0300

    fix semistandard requeriments

--- 0x15-javascript-web_jquer/7-script.js
+++ 0x15-javascript-web_jquer/7-script.js
@@ -1,3 +1,3 @@
-jQuery.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function( data ) {
-      $('DIV#character').html(data.name);
+$.jQuery.getJSON('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
+  $('DIV#character').html(data.name);
 });
