629358da80b66a01cf89b2d22d92f3e7ac040647
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-05-12 22:16:18 -0300

    task 8

---
+++ 0x15-javascript-web_jquer/8-script.js
@@ -0,0 +1,5 @@
+$.get("https://swapi-api.hbtn.io/api/films/?format=json", function(data) {
+    for (const index in data.results) {
+        $('UL#list_movies').append('<li>' + data.results[index].title + '</li>');
+    }
+});
